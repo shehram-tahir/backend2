f6fe06134930a76546dfa964b79bd5641481f6e9
abdullah abbas <abdullahalhoothy92@gmail.com>
2024-09-02 20:14:23 -0400

    seperated secrets.json

--- config_factory.py
+++ config_factory.py
@@ -105,11 +105,15 @@ class ConfigFactory:
 def get_conf() -> static_ApiConfig:
     conf = static_ApiConfig()
     try:
-        with open("secrets/secrets.json", "r", encoding="utf-8") as config_file:
+        with open("secrets/secrets_gmap.json", "r", encoding="utf-8") as config_file:
             data = json.load(config_file)
             conf.api_key = data.get("gmaps_api", "")
+
+        with open("secrets/secrets_firebase.json", "r", encoding="utf-8") as config_file:
+            data = json.load(config_file)
             conf.firebase_api_key = data.get("firebase_api_key", "")
             conf.firebase_sp_path = data.get("firebase_sp_path", "")
+            
         return conf
     except Exception as e:
         return conf
